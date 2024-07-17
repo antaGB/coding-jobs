95c878c74b1b39fa896d5099dc51f74a935c7d01
antaGB <antapermana1156@gmail.com>
2024-07-17 22:08:21 +0800

    creating userprofile models and dashboard view

--- codingjobs/urls.py
+++ codingjobs/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views
 
 from apps.core.views import frontPage, signup
 from apps.job.views import job_detail
+from apps.userprofile.views import dashboard
 
 
 urlpatterns = [
@@ -28,6 +29,7 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('admin/', admin.site.urls),
+    path('dashboard/', dashboard, name='dashboard'),
 
     path('jobs/<int:job_id>', job_detail, name='job_detail'),
 ]
